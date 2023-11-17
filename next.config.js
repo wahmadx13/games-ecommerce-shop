497ac3f79d3c00b358b20ff23596fbbed0176d64
Waseem <wasim_ahmad2003@yahoo.com>
2023-11-17 23:30:40 +0500

    a minor change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com", "unsplash.com"],
+    domains: [
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "unsplash.com",
+      "wallpaperset.com",
+    ],
   },
 };
 
