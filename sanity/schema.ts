27f3605015e8c232a84b9160e64f26fc00c31254
Waseem <wasim_ahmad2003@yahoo.com>
2023-11-17 14:42:27 +0500

    created schema for categories

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from "sanity";
+import category from "./schemas/category";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [category],
 };
---
+++ sanity/schemas/category.ts
@@ -0,0 +1,28 @@
+// Schema for Category
+
+const category = {
+  title: "Category",
+  name: "category",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      validation: (rule: any) => rule.required(),
+    },
+    {
+      name: "slug",
+      type: "slug",
+      option: { source: "name" },
+      validation: (rule: any) => rule.required(),
+    },
+    { name: "image", type: "url", validation: (rule: any) => rule.required() },
+    {
+      name: "subtitle",
+      type: "string",
+      validation: (rule: any) => rule.required(),
+    },
+  ],
+};
+
+export default category;
