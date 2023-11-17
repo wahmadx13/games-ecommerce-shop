2b629769ed4074a1171d52995d5ef2e710516b2a
Waseem <wasim_ahmad2003@yahoo.com>
2023-11-17 16:07:20 +0500

    query games from sanity

--- libs/api.ts
+++ libs/api.ts
@@ -1,5 +1,6 @@
 import sanityClient from "./sanity";
 import { Category } from "@/types/category";
+import { Game } from "@/types/game";
 
 export const getCategories = async (): Promise<Category[]> => {
   const query = `*[_type == "category"] {
@@ -12,4 +13,25 @@ export const getCategories = async (): Promise<Category[]> => {
   const categories: Category[] = await sanityClient.fetch({ query });
 
   return categories;
+};
+
+export const getGames = async (): Promise<Game[]> => {
+  const query = `*[_type == "game"] {
+        name,
+        price,
+        images,
+        isFeatured,
+        isTrending,
+        'category': *[_id == ^.category_ref][0] {
+            name,
+            slug {
+                current
+            }
+        },
+        slug,
+        quantity,
+        description
+    }`;
+  const games: Game[] = await sanityClient.fetch({ query });
+  return games;
 };
