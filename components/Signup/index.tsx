f739aef72bf0af3c2175b347841ca7681c1f955a
Waseem <wasim_ahmad2003@yahoo.com>
2023-11-18 17:05:16 +0500

    integrate sign up logic integrate mongodb and sync signup user with mongodb

--- components/Signup/index.tsx
+++ components/Signup/index.tsx
@@ -1,9 +1,41 @@
-import { FC } from "react";
+"use client";
+import { FC, useRef, useState } from "react";
+import axios from "axios";
 import signupClassNames from "./signupClassNames";
 import { SignupProps } from "@/types/signupProps";
 
 const SignUp: FC<SignupProps> = (props) => {
   const { isSignupFormOpen, toggleForm } = props;
+
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleCloseForm = () => {
+    toggleForm();
+  };
+
+  const signupHandler = async () => {
+    if (!emailRef.current || !passwordRef.current) return;
+    setIsFormSubmitting(true);
+    try {
+      const response = await axios.post("/api/sign-up", {
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+
+      setIsFormSubmitting(false);
+      handleCloseForm();
+
+      if (response.data) window.alert("Sign up success. Please Sign in");
+    } catch (error) {
+      setIsFormSubmitting(false);
+      handleCloseForm();
+      console.log("error", error);
+    }
+  };
+
   const {
     container,
     card,
@@ -16,7 +48,7 @@ const SignUp: FC<SignupProps> = (props) => {
     confirm,
   } = signupClassNames;
   return isSignupFormOpen ? (
-    <div className={container}>
+    <div onClick={handleCloseForm} className={container}>
       <div className={card}>
         <h2 className={heading}>Sign up</h2>
         <form>
@@ -25,6 +57,7 @@ const SignUp: FC<SignupProps> = (props) => {
               Email
             </label>
             <input
+              ref={emailRef}
               type="email"
               className={input}
               id="email"
@@ -36,6 +69,7 @@ const SignUp: FC<SignupProps> = (props) => {
               Password
             </label>
             <input
+              ref={passwordRef}
               type="password"
               className={input}
               id="password"
@@ -43,8 +77,15 @@ const SignUp: FC<SignupProps> = (props) => {
             />
           </div>
           <div className={btnContainer}>
-            <span className={cancel}>Cancel</span>
-            <button className={confirm} type="button">
+            <span onClick={handleCloseForm} className={cancel}>
+              Cancel
+            </span>
+            <button
+              disabled={isFormSubmitting}
+              onClick={signupHandler}
+              className={confirm}
+              type="button"
+            >
               Sign Up
             </button>
           </div>
